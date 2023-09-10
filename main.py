dd10d6b02705bde493ed728a8086605c46150258
thuku <thukubeth13@gmail.com>
2023-09-10 18:51:16 +0300

    test

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+from datetime import datetime
+import pytz
+
+app = FastAPI()
+
+@app.get("/api")
+async def get_endpoint(slack_name: str, track: str):
+    # Current UTC time with a +/-2 minute window
+    utc_time = datetime.now(pytz.utc)
+
+    #JSON format response
+    response = {
+        "slack_name": slack_name,
+        "day_of_week": utc_time.strftime("%A"), 
+        "current_utc_time": utc_time.strftime("%Y-%m-%dT%H:%M:%S"),
+        "track": track,
+        "github_file_url": "https://github.com/lizawt/hngxstg1/..",
+        "github_repo_url": "https://github.com/lizawt/hngxstg1",
+        "status_code": 200
+    }
+
+    return response
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
